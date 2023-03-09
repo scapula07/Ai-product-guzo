e3400b2105a20bbd2fb574db705db63465333010
francis <cjfrancisf@gmail.com>
2023-03-09 22:14:27 +0100

    new

--- src/components/dashboard/DashboardRoutes.js
+++ src/components/dashboard/DashboardRoutes.js
@@ -57,7 +57,7 @@ const DashboardRoutes = () => {
         let path = "/dashboard/create-community-profile";
         if (
           window.location.href !==
-          "http://localhost:3000/dashboard/create-community-profile"
+          "https://guzo-dev.vercel.app/dashboard/create-community-profile"
         
         ) {
         if(!community.label){
