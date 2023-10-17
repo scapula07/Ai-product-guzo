924b649c437cacee7f57320a59c3aab91cc6fe23
scapula07 <bartholomewonogwu@yahoo.com>
2023-10-17 18:53:20 +0100

    attach tab indicator

--- src/modules/Connections/components/tabs.jsx
+++ src/modules/Connections/components/tabs.jsx
@@ -2,21 +2,27 @@ import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Tabs() {
-  const [active,setActive]=useState("activee")
+   const [active,setActive]=useState("activee")
+     const currentURL = window.location.href;
+    console.log(currentURL,"urlll")
+
+    const parts = currentURL?.split('/');
+    const lastPart = parts[parts.length - 1];
+    console.log(lastPart,"lastp"); // "ecosystems"
   return (
     <div className='bg-white w-full rounded-lg flex items-center    px-8 py-2'>
         <div className='w-1/2 flex justify-center'
            onClick={()=>setActive("activee")}
         >
            <Link to="">
-            <h5 className={`${active==="activee" ?'text-sm font-semibold  border-blue-600 ':'text-sm font-semibold'}`}>Active</h5>
+            <h5 className={`${lastPart !="pending" ?'text-sm font-semibold  border-blue-600 border-b-2':'text-sm font-semibold'}`}>Active</h5>
             </Link>
         </div>
           <div className='w-1/2 flex justify-center'
               onClick={()=>setActive("pending")}
           >
           <Link to="pending">
-            <h5  className={`${active==="pending" ?'text-sm font-semibold  border-blue-600 ':'text-sm font-semibold'}`}>Pending</h5>
+            <h5  className={`${lastPart =="pending" ?'text-sm font-semibold  border-blue-600 border-b-2 ':'text-sm font-semibold'}`}>Pending</h5>
             </Link>
           </div>
       
--- src/modules/Home/components/tabs.jsx
+++ src/modules/Home/components/tabs.jsx
@@ -2,14 +2,24 @@ import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Tabs() {
+
+    console.log("Tabbbbbbbbsss")
     const [active,setActive]=useState("feeds")
+    const currentURL = window.location.href;
+    console.log(currentURL,"urlll")
+
+      const parts = currentURL?.split('/');
+      const lastPart = parts[parts.length - 1];
+      console.log(lastPart,"lastp"); // "ecosystems"
+
+ 
   return (
     <div className='bg-white w-full rounded-lg flex items-center    px-8 py-2'>
         <div className='w-1/2 flex justify-center'
            onClick={()=>setActive("feeds")}
         >
             <Link to="">
-               <h5 className={`${active==="feeds" ?'text-sm font-semibold  border-blue-600 ':'text-sm font-semibold'}`} >Feed</h5>
+               <h5 className={`${lastPart !="ecosystems" ?'text-sm font-semibold  border-blue-600 border-b-2':'text-sm font-semibold'}`} >Feed</h5>
             </Link>
            
         </div>
@@ -17,7 +27,7 @@ export default function Tabs() {
            onClick={()=>setActive("ecos")}
         >
             <Link to="ecosystems">
-              <h5 className={`${active==="ecos" ?'text-sm font-semibold  border-blue-600 ':'text-sm font-semibold'}`}>Ecosystem</h5>
+              <h5 className={`${lastPart==="ecosystems" ?'text-sm font-semibold  border-blue-600 border-b-2':'text-sm font-semibold'}`}>Ecosystem</h5>
            </Link>
         </div>
       
