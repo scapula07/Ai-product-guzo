9aef880c7ced0650a3382189e29996d0f13fb305
scapula07 <bartholomewonogwu@yahoo.com>
2023-11-04 13:01:37 +0100

    added settings for different env

--- src/modules/Firebase/index.js
+++ src/modules/Firebase/index.js
@@ -10,9 +10,8 @@ var url = new URL(currentURL);
 var domainName = url.hostname;
 
 
-console.log(currentURL,url,domainName,"addressss")
 
-const firebaseConfig = {
+let firebaseConfig ={
   apiKey: "AIzaSyBCCrk78nJ-ipfhxDlodPoUlSgJSUP_jTo",
   authDomain: "guzo-v2.firebaseapp.com",
   projectId: "guzo-v2",
@@ -23,15 +22,36 @@ const firebaseConfig = {
 };
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCoMTJsWVIdi5TsqXwDI620Y6Y89a-Hg4M",
-//   authDomain: "guzo-sandbox.firebaseapp.com",
-//   projectId: "guzo-sandbox",
-//   storageBucket: "guzo-sandbox.appspot.com",
-//   messagingSenderId: "1025453001887",
-//   appId: "1:1025453001887:web:0a5096f66c6022221566aa",
-//   measurementId: "G-5HZG9YS53K"
-// };
+if(domainName==="community.guzo.io"){
+   firebaseConfig = {
+    apiKey: "AIzaSyBCCrk78nJ-ipfhxDlodPoUlSgJSUP_jTo",
+    authDomain: "guzo-v2.firebaseapp.com",
+    projectId: "guzo-v2",
+    storageBucket: "guzo-v2.appspot.com",
+    messagingSenderId: "573309380008",
+    appId: "1:573309380008:web:0ed1de3acffe7775ae299d",
+    measurementId: "G-9TFLFQMJCH"
+  };
+
+}else{
+   firebaseConfig = {
+      apiKey: "AIzaSyCoMTJsWVIdi5TsqXwDI620Y6Y89a-Hg4M",
+      authDomain: "guzo-sandbox.firebaseapp.com",
+      projectId: "guzo-sandbox",
+      storageBucket: "guzo-sandbox.appspot.com",
+      messagingSenderId: "1025453001887",
+      appId: "1:1025453001887:web:0a5096f66c6022221566aa",
+      measurementId: "G-5HZG9YS53K"
+  };
+
+}
+
+
+console.log(currentURL,url,domainName,firebaseConfig?.projectId,"addressss")
+
+
+
+
 
 
   
